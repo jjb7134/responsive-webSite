a9dc59d0bf34a0fd2b087429a4641cfa7a7c8ac0
firely95 <90025832+jjb7134@users.noreply.github.com>
2022-03-10 16:03:31 +0900

    스크롤 버그 수정
    scroll 이동 시 scetion active 버그 수정

--- js/main.js
+++ js/main.js
@@ -134,24 +134,24 @@ let swiper = new Swiper(".portfolio__container", {
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 // 해당페이지 위치할때 아이콘에 호버효과 + 스크롤중 페이지 위치에 아이콘 호버효과
-// const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll('section[id]')
 
-// function scrollActive(){
-//     const scrollY = window.pageYOffset
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
-//     sections.forEach(current =>{
-//         const sectionHeight = current.offsetHeight
-//         const sectionTop = current.offsetTop - 50;
-//         sectionId = current.getAttribute('id')
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
 
-//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-//         }else{
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-//         }
-//     })
-// }
-// window.addEventListener('scroll', scrollActive)
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
